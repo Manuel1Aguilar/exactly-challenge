24b8d6cd6604c96f2023febd937b119b089656c2
Manuel1Aguilar <manuelmla.aguilar@gmail.com>
2022-04-10 19:44:54 -0300

    Added events and comments to ETHPool.sol

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 
 // 1) Setup a project and create a contract
 // Summary
@@ -16,14 +16,52 @@ pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
 
+/**
+    @author Manuel Aguilar
+    @title ETHPool
+    @notice A contract that allows users to have a share on an ETH pool and receive rewards based on the share of
+    the pool they own
+ */
+
 contract ETHPool {
+    /**
+        @notice Struct to save a user's current share of the pool
+     */
     struct Share {
         address owner;
         uint256 absValue;
     }
+    /**
+        @notice Stores the addreses that can deposit a reward
+     */
     mapping(address => bool) public teamAddresses;
+    /**
+        @notice Current balance of the contract
+     */
     uint256 public contractBalance;
+    /**
+        @notice Array of user's shares on the contract
+     */
     Share[] public shareBalances;
+
+    /**
+        @notice Set the teamAddresses mapping
+     */
+     
+    /**
+        @notice Event emitted when someone deposits
+        @param depositer The address that has made the deposit
+        @param amount Amount deposited in WEIs
+        @param isReward true if this deposit has been distributed as a reward to shareholders
+     */
+    event Deposit(address indexed depositer, uint256 amount, bool isReward);
+
+    /**
+        @notice Event emitted when someone withdraws their share
+        @param shareholder The address that withdrew their share
+        @param amount Amount withdrawn in WEIs
+     */
+    event Withdrawn(address indexed shareholder, uint256 amount);
 
     constructor(address[] memory _teamAddresses) {
         console.log("Deploying an ETHPool contract with team size:", _teamAddresses.length);
@@ -36,6 +74,12 @@ contract ETHPool {
     // implemented.
     // There may be an overflow problem too when calculating the reward but I'm not sure it's possible
     // to reach the wei values to overflow uint256
+    
+    /**
+        @notice Function that lets users deposit and if they are a team member as per the teamAddresses mapping their
+        deposit will be distributed as a reward to all current user shares, if not, it will be added as their share 
+        of the contract. In any case, the deposit will be added to the contract balance.
+     */
     function deposit() public payable {
         if(teamAddresses[msg.sender] == true){
             // Calculate and deposit rewards
@@ -64,6 +108,9 @@ contract ETHPool {
         contractBalance += msg.value;
     }
 
+    /**
+        @notice Function that lets a user withdraw their share from the contract
+     */
     function withdraw() public{
         //Loop through share array and send corresponding absValue
         uint256 withdrawValue;
@@ -79,6 +126,10 @@ contract ETHPool {
         require(sent, "Failed to send Ether");
     }
 
+    /**
+        @notice Function that lets a user check the current share balance of an address
+        on this contract.
+     */
     function balanceOf(address _address) public view returns (uint){
         //Loop through share array and return abs value
         for (uint256 index = 0; index < shareBalances.length; index++) {
